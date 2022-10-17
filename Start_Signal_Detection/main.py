23aed16d02107a50f75348d423547da8c8bdb506
Sterley Labady <43834576+Sterley@users.noreply.github.com>
2022-10-17 17:17:36 +0200

    Create main.py

---
+++ Start_Signal_Detection/main.py
@@ -0,0 +1,13 @@
+from sympy import false
+from utils import *
+
+def main ():
+
+    while(1):
+
+        if have_to_go():
+            eyebrow_up()
+        else:
+            eyebrow_down()
+
+main()
